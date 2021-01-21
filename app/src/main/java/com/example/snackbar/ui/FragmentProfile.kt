75354eac3ceb4db1784a448d0040768f4bfe6906
sarafelix25 <sara.felix@aluno.ufabc.edu.br>
2021-01-21 20:10:20 -0300

    first

---
+++ app/src/main/java/com/example/snackbar/ui/FragmentProfile.kt
@@ -0,0 +1,33 @@
+package com.example.snackbar.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.snackbar.R
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+
+
+class FragmentProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        view.button.setOnClickListener() {
+
+            findNavController().navigate(R.id.action_fragmentProfile_to_fragmentAjuda)
+        }
+
+
+        return view
+    }
+
+
+}
